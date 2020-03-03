82ddf3e6f373703a7fd39d5e094c2916858e83d9
Dustin Mikusko <dustinjm713@yahoo.com>
2020-03-03 15:52:06 -0700

    Style century buttons

--- src/Century.svelte
+++ src/Century.svelte
@@ -11,4 +11,22 @@
 </script>
 
 
-<button type="button" on:click={chooseCentury}>{name}</button>
+<button type="button" class="century-btn" on:click={chooseCentury}>{name}</button>
+
+<style>
+  .century-btn {
+    background-color: rgb(53, 175, 220);
+    color: white;
+    margin: 3px;
+    border: none;
+  }
+
+  .century-btn:hover {
+    background-color: rgb(35, 146, 186);
+    cursor: pointer;
+  }
+
+  .century-btn:active {
+    transform: scale(.97);
+  }
+</style>
