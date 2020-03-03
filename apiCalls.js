7b3c389605149f21e53e3bc375896cb08af3972d
Dustin Mikusko <dustinjm713@yahoo.com>
2020-03-03 08:23:24 -0700

    Add apiCalls to api

---
+++ apiCalls.js
@@ -0,0 +1,19 @@
+export const fetchAllCenturies = () => {
+  return fetch('https://api.harvardartmuseums.org/century?size=47&apikey=7416b440-5c9c-11ea-9401-ed9c8b78a90d')
+    .then(res => {
+      if (!res.ok) {
+        throw Error('Error getting centuires')
+      }
+      return res.json();
+    })
+};
+
+export const fetchObjectsFromCentury = century => {
+  return fetch(`https://api.harvardartmuseums.org/object?century=${century}&size=20&apikey=7416b440-5c9c-11ea-9401-ed9c8b78a90d`)
+    .then(res => {
+      if (!res.ok) {
+        throw Error('Error getting objects')
+      }
+      return res.json();
+    })
+};
